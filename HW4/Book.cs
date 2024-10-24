6d04b14c0fe090c0007750e8d8eaa6f7e357571a
alitn2000 <alitn2000@gmail.com>
2024-10-24 09:49:51 +0330

    first commit for HW4

---
+++ HW4/Book.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Diagnostics;
+
+namespace HW4;
+
+internal class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public int Pages { get; set; }
+    public string Genre { get; set; }
+    public int PublishedYear { get; set; }
+
+    private double Price;
+
+    public double price
+    {
+        get { return Price; }
+
+    }
+    public void SetPrice(double newPrice)
+    {
+        if (newPrice >= 0)
+        {
+            Price = newPrice;
+        }
+        else
+        {
+            throw new ArgumentException("Price cannot be negative.");
+        }
+    }
+    public Book(string title, string author, string publisher, int pages, double initialPrice, string genre, int publishedyear)
+    {
+
+        Title = title;
+        Author = author;
+        Publisher = publisher;
+        Pages = pages;
+        Price = price;
+        Genre = genre;
+        PublishedYear = publishedyear;
+        SetPrice(initialPrice);
+
+    }
+
+    public void show()
+    {
+        Console.WriteLine($"Title: {Title}\nAuthor: {Author}\nPublisher: {Publisher}\nYear: {PublishedYear}\n Price: {price}");
+        Console.WriteLine("--------------------------------------------------------------------------------------------------");
+    }
+
+    public void PricePlus (double p)
+    {
+        price += p;
+    }
+
+    public List<Book> GetBooks(Book[] books, string author)
+    {
+        List<Book> authors = new List<Book>();
+
+        foreach (var b in books)
+        {
+            if (b.Author == author) 
+            {
+                authors.Add(b);
+            }
+        }
+
+        return authors;
+    }
+
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,24 @@
+﻿using HW4;
+
+Book[] books = new Book[]
+      {
+            new Book("book1", "A", "Publisher1", 200, 1000, "Novel", 2020),
+            new Book("book2", "B", "Publisher2", 201, 2000, "Fiction", 2004),
+            new Book("book3", "C", "Publisher3", 202, 3000, "Dystopian", 1999),
+            new Book("book4", "C", "Publisher4", 203, 4000, "Satire", 1987)
+      };
+foreach (var book in books)
+{
+    book.show();
+}
+books[0].PricePlus(10000);
+Console.WriteLine("***************************Plus***************************:");
+books[0].show();
+
+Book bookObject = new Book("", "", "", 0, 0, "", 0);
+Console.WriteLine("***************************publisher (C):***************************");
+List<Book> c = bookObject.GetBooks(books, "C");
+foreach (var book in c)
+{
+    book.show();
+}
